3f818028e69c3c05f54fc78a51ae738b4ba82d75
Ryan Kelly <94ryan.kelly@gmail.com>
2022-04-04 18:40:39 +0100

    Sample source generator setup

---
+++ test/Parquet.SourceGenerator.CLI/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Parquet.SourceGenerator.CLI;
+
+partial class Program
+{
+    static void Main(string[] args)
+    {
+        HelloFrom("Generated Code");
+    }
+
+    static partial void HelloFrom(string name);
+}
